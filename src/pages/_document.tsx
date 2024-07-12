593e923eb9209f4b6c8e18aba64c7721e2a52102
Moyses-jr <83251860+Moyses-jr@users.noreply.github.com>
2024-07-12 07:42:51 -0300

    Ajustes front

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Transporte Estudantes</title>
       <link rel="shortcut icon" href="../../public/icon.png" />
       <body className="bg-blue-600 bg-opacity-25">
         <Main />
